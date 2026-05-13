15ac7c7813af89434388693664fe60a8349bf0e3
fbcs-jourdan <2260504@flourbluffschools.org>
2026-05-13 12:52:55 -0500

    Add warning sign and timed rock spawn logic
    Add a warning_sign texture asset and import settings; introduce a Sprite3D warning sign and separate timers in the RockSpawner scene to show a warning before spawning. Rework rock_spawner.gd to move the warning to a random spawn position, toggle timers, and spawn rocks at the warning location; rename timer nodes and update connections. Update rock.gd to rotate falling rocks during physics processing. Minor scene housekeeping: switch rock.tscn to instantiate scenes/rock_4.tscn, adjust node unique IDs and rock_4 light energy, and set 3D default gravity to 3.0 in project settings.

--- first-ascent/scripts/rock_spawner.gd
+++ first-ascent/scripts/rock_spawner.gd
@@ -1,15 +1,29 @@
 extends Node3D
 @onready var spawn_positions: Node3D = $SpawnPositions
+@onready var warning_sign: Sprite3D = $warning_sign
+@onready var rock_timer: Timer = $RockTimer
+@onready var warning_timer: Timer = $WarningTimer
 
 var rock_scene = preload("res://scenes/rock.tscn")
-
-func _on_timer_timeout() -> void:
-	spawn_rock()
-	print("im matthew and im psawning a rock")
 	
 func spawn_rock() -> void:
 	var spawn_positions_array = spawn_positions.get_children()
 	var random_spawn_position = spawn_positions_array.pick_random()
 	var rock_instance = rock_scene.instantiate()
 	add_child(rock_instance)
-	rock_instance.global_position = random_spawn_position.global_position
+	rock_instance.global_position = warning_sign.global_position
+	
+	
+func _on_rock_timer_timeout() -> void:
+	spawn_rock()
+	print("im matthew and im spawning a rock")
+	warning_timer.start()
+
+func move_warning() -> void:
+	var spawn_positions_array = spawn_positions.get_children()
+	var random_spawn_position = spawn_positions_array.pick_random()
+	warning_sign.global_position.x = random_spawn_position.global_position.x
+	rock_timer.start()
+	
+func _on_warning_timer_timeout() -> void:
+	move_warning()
