b2c20319c1d267f4420cbcd8695a4ca01741cfb0
fbcs-jourdan <2260504@flourbluffschools.org>
2026-05-13 13:09:51 -0500

    Nest warning_sign under Control; update spawner
    Move the warning_sign Sprite3D into a new Control node (and add a Container) in the rock_spawner scene to enable UI layout/anchoring. Update rock_spawner.gd to use the new node paths (Control/Container and Control/warning_sign) and add an onready reference for the container. Adjust spawn_rock to only copy the warning_sign's x position to the spawned rock (previous full-position assignment removed) and retain a commented-out y-check/queue_free for future handling.

--- first-ascent/scripts/rock_spawner.gd
+++ first-ascent/scripts/rock_spawner.gd
@@ -1,18 +1,23 @@
 extends Node3D
 @onready var spawn_positions: Node3D = $SpawnPositions
-@onready var warning_sign: Sprite3D = $warning_sign
 @onready var rock_timer: Timer = $RockTimer
 @onready var warning_timer: Timer = $WarningTimer
+@onready var container: Container = $Control/Container
+@onready var warning_sign: Sprite3D = $Control/warning_sign
 
 var rock_scene = preload("res://scenes/rock.tscn")
+
+	
+
 	
 func spawn_rock() -> void:
 	var spawn_positions_array = spawn_positions.get_children()
 	var random_spawn_position = spawn_positions_array.pick_random()
 	var rock_instance = rock_scene.instantiate()
 	add_child(rock_instance)
-	rock_instance.global_position = warning_sign.global_position
-	
+	rock_instance.global_position.x = warning_sign.global_position.x
+	#if rock_instance.global_position.y <= 0:
+		#rock_instance.queue_free()
 	
 func _on_rock_timer_timeout() -> void:
 	spawn_rock()
