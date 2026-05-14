d26c0b07a2a2b860edabbf42a5e9906e1f1e6d1d
fbcs-jourdan <2260504@flourbluffschools.org>
2026-05-14 11:55:14 -0500

    game is COMPLETELY BROKEN

--- first-ascent/scripts/rock_spawner.gd
+++ first-ascent/scripts/rock_spawner.gd
@@ -10,13 +10,19 @@ var rock_scene = preload("res://scenes/rock.tscn")
 
 	
 
+
+
+
+func _on_timer_timeout() -> void:
+	spawn_rock()
+	print("im matthew and im psawning a rock")
 	
 func spawn_rock() -> void:
 	var spawn_positions_array = spawn_positions.get_children()
 	var random_spawn_position = spawn_positions_array.pick_random()
 	var rock_instance = rock_scene.instantiate()
 	add_child(rock_instance)
-	rock_instance.global_position.x = warning_sign.global_position.x
+
 	#if rock_instance.global_position.y <= 0:
 		#rock_instance.queue_free()
 	
@@ -33,3 +39,7 @@ func move_warning() -> void:
 	
 func _on_warning_timer_timeout() -> void:
 	move_warning()
+	var spawn_positions_array = spawn_positions.get_children()
+	var random_spawn_position = spawn_positions_array.pick_random()
+	var rock_instance = rock_scene.instantiate()
+	rock_instance.global_position = random_spawn_position.global_position
